398bf55e071c80b148fe55c2ad0a32f5942a89eb
Denis Muftic <theprike4@gmail.com>
2025-11-01 19:54:38 +0100

    implement validateConnection

--- Classes/MSSQLServerConnection.cs
+++ Classes/MSSQLServerConnection.cs
@@ -1,12 +1,36 @@
-﻿using EZBank.Interfaces;
+﻿using EZBank.Helpers;
+using EZBank.Interfaces;
+using System.Data.SqlClient;
 
 namespace EZBank.Classes
 {
     public class MSSQLServerConnection : IDBServerConnection
     {
+        private ISession _session;
+
         public bool ValidateConnection(ISession session)
         {
-            return false;
+            try
+            {
+                _session = session;
+                using (SqlConnection connection = new SqlConnection(CreateConnectionString()))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
         }
+
+        private string CreateConnectionString()
+        {
+            return $"Server={_session.serverName};User Id={_session.userName};Password={_session.password}";
+        }
+
+
     }
 }
